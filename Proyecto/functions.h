6efebebf382da383d8f0f75156b668991bccce3b
jesusurdiales <jesus.urdiales93@gmail.com>
2014-11-24 10:54:42 +0100

    norma, posibilidad de múltiples plazas

--- Proyecto/functions.h
+++ Proyecto/functions.h
@@ -8,6 +8,7 @@
 #include <opencv2\opencv.hpp>
 #include <opencv2\video\tracking.hpp>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 using namespace cv;
--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -4,7 +4,7 @@
 
 #define camara 1
 #define dimensionesROI 15
-#define umbralPresencia 100
+#define umbralPresencia 200
 
 /* DECLARACIÓN DE FUNCIONES GLOBALES */
 void onMouse(int event, int x, int y, int, void*);
@@ -14,16 +14,19 @@ void onMouse(int event, int x, int y, int, void*);
 // Puntos donde se almacenan las coordenadas del pixel en el cual estamos haciendo click
 Point pulsar, soltar;
 // Plaza donde estará aparcado el coche
-Rect plaza;
+vector<Rect> plaza;
+Rect seleccion;
 // Variable que informa al bucle principal que ya hemos seleccionado todas las plazas
+Mat frame;
 bool plazaSeleccionada = false;
 int numeroPlazas = 1;
+int plazasSeleccionadas = 0;
 
 int main()
 {
 	/* INICIALIZACIÓN DEL PROGRAMA Y ACCESO A LA CÁMARA */
 
-	Mat frame, frame_gray, umbral(frame.size(), CV_8U);
+	Mat frame_gray, umbral(frame.size(), CV_8U);
 	char esc;
 
 	std::cout << endl << "Inacializacion del programa. Accediendo a la camara...." << endl;
@@ -36,6 +39,12 @@ int main()
 		getchar();
 		return(EXIT_FAILURE);
 	}
+
+	// Selección del número de plazas
+	cout << endl << "Introduzca el numero de plazas: ";
+	cin >> numeroPlazas;
+
+	plaza.resize(numeroPlazas);
 
 	// Ventanas para mostrar los resultados
 	namedWindow("camara", 1);
@@ -113,7 +122,7 @@ int main()
 		// Podemos salir del bucle pulsando cualquier tecla manualmente o cuando haya finalizado la selección de plazas sale 
 		// automáticamente. Para salir del programa hay que pulsar la tecla esc
 		esc = cv::waitKey(30);
-		if ( (plazaSeleccionada == true) )
+		if ( numeroPlazas == plazasSeleccionadas )
 			break;
 		if (esc == 27)
 			return 0;
@@ -122,64 +131,32 @@ int main()
 	destroyAllWindows();
 
 	/* CREACIÓN DE LAS REGIONES DE INTERÉS (ROIs) */
-	Rect ROI1(0, 0, 0, 0), ROI2(0, 0, 0, 0);
+	vector<Rect> ROIs1;
+	vector<Rect> ROIs2;
 
-	// Antes de nada es necesario comprobar si la plaza está en horizontal o vertical respecto a la cámara
-	if (plaza.width <= plaza.height)
+	// Antes de nada es necesario comprobar si cada plaza está en horizontal o vertical respecto a la cámara
+	for (int i = 0; i < numeroPlazas; i++)
 	{
-		// Si el rectángulo tiene más de 10x20 píxeles podemos crear dentro dos rectángulos de 15x15
-		if (plaza.width > 20 && plaza.height > 40)
-		{
-			int x0, x2, y2, y0;
-			// Calculamos las posiciones de las ROIs y sus dimensiones
-			x2 = plaza.x + plaza.width;
-			y2 = plaza.y + plaza.height;
-			x0 = cvRound(plaza.x + (x2 - plaza.x) / 2 - (dimensionesROI / 2));
-			y0 = cvRound(plaza.y + (y2 - plaza.y) / 4 - (dimensionesROI / 2));
-			// Calculamos ROI1 y ROI2
-			ROI1 = Rect(x0, y0, dimensionesROI, dimensionesROI);
-			y0 = cvRound(plaza.y + 3 * (y2 - plaza.y) / 4 - (dimensionesROI / 2));
-			ROI2 = Rect(x0, y0, dimensionesROI, dimensionesROI);
-		}
-		else
-		{
-			// Si el tamaño de la plaza es demasiado pequeño, el algoritmo no va a funcionar bien, por lo que se desecha
-			if ((plaza.width < dimensionesROI) || (plaza.height < dimensionesROI))
-			{
-				cout << "La plaza seleccionada es demasiado pequeña para el correcto funcionamiento del algoritmo." << endl;
-				cout << "Fin del programa" << endl;
-				getchar();
-				exit(EXIT_FAILURE);
-			}
-			// Solo tendríamos en este caso una ROI en el centro de la plaza
-			int x0, y0;
-			x0 = cvRound(plaza.x + plaza.width / 2 - dimensionesROI / 2);
-			y0 = cvRound(plaza.y + plaza.height / 2 - dimensionesROI / 2);
-			ROI1 = Rect(x0, y0, dimensionesROI, dimensionesROI);
-		}
-	}
-	else
-	// La plaza está horizontal respesto a la cámara. El código será igual que el anterior, pero modificando la anchura por altura
-	{
+		if (plaza[i].width <= plaza[i].height)
 		{
 			// Si el rectángulo tiene más de 10x20 píxeles podemos crear dentro dos rectángulos de 15x15
-			if (plaza.height > 20 && plaza.width > 40)
+			if (plaza[i].width > 20 && plaza[i].height > 40)
 			{
 				int x0, x2, y2, y0;
 				// Calculamos las posiciones de las ROIs y sus dimensiones
-				x2 = plaza.x + plaza.width;
-				y2 = plaza.y + plaza.height;
-				x0 = cvRound(plaza.x + (x2 - plaza.x) / 4 - (dimensionesROI / 2));
-				y0 = cvRound(plaza.y + (y2 - plaza.y) / 2 - (dimensionesROI / 2));
+				x2 = plaza[i].x + plaza[i].width;
+				y2 = plaza[i].y + plaza[i].height;
+				x0 = cvRound(plaza[i].x + (x2 - plaza[i].x) / 2 - (dimensionesROI / 2));
+				y0 = cvRound(plaza[i].y + (y2 - plaza[i].y) / 4 - (dimensionesROI / 2));
 				// Calculamos ROI1 y ROI2
-				ROI1 = Rect(x0, y0, dimensionesROI, dimensionesROI);
-				x0 = cvRound(plaza.x + 3 * (x2 - plaza.x) / 4 - (dimensionesROI / 2));
-				ROI2 = Rect(x0, y0, dimensionesROI, dimensionesROI);
+				ROIs1.push_back(Rect(x0, y0, dimensionesROI, dimensionesROI));
+				y0 = cvRound(plaza[i].y + 3 * (y2 - plaza[i].y) / 4 - (dimensionesROI / 2));
+				ROIs2.push_back(Rect(x0, y0, dimensionesROI, dimensionesROI));
 			}
 			else
 			{
 				// Si el tamaño de la plaza es demasiado pequeño, el algoritmo no va a funcionar bien, por lo que se desecha
-				if ((plaza.width < dimensionesROI) || (plaza.height < dimensionesROI))
+				if ((plaza[i].width < dimensionesROI) || (plaza[i].height < dimensionesROI))
 				{
 					cout << "La plaza seleccionada es demasiado pequeña para el correcto funcionamiento del algoritmo." << endl;
 					cout << "Fin del programa" << endl;
@@ -188,28 +165,69 @@ int main()
 				}
 				// Solo tendríamos en este caso una ROI en el centro de la plaza
 				int x0, y0;
-				x0 = cvRound(plaza.x + plaza.width / 2 - dimensionesROI / 2);
-				y0 = cvRound(plaza.y + plaza.height / 2 - dimensionesROI / 2);
-				ROI1 = Rect(x0, y0, dimensionesROI, dimensionesROI);
+				x0 = cvRound(plaza[i].x + plaza[i].width / 2 - dimensionesROI / 2);
+				y0 = cvRound(plaza[i].y + plaza[i].height / 2 - dimensionesROI / 2);
+				ROIs1.push_back(Rect(x0, y0, dimensionesROI, dimensionesROI));
+			}
+		}
+		else
+			// La plaza está horizontal respesto a la cámara. El código será igual que el anterior, pero modificando la anchura por altura
+		{
+			{
+				// Si el rectángulo tiene más de 10x20 píxeles podemos crear dentro dos rectángulos de 15x15
+				if (plaza[i].height > 20 && plaza[i].width > 40)
+				{
+					int x0, x2, y2, y0;
+					// Calculamos las posiciones de las ROIs y sus dimensiones
+					x2 = plaza[i].x + plaza[i].width;
+					y2 = plaza[i].y + plaza[i].height;
+					x0 = cvRound(plaza[i].x + (x2 - plaza[i].x) / 4 - (dimensionesROI / 2));
+					y0 = cvRound(plaza[i].y + (y2 - plaza[i].y) / 2 - (dimensionesROI / 2));
+					// Calculamos ROI1 y ROI2
+					ROIs1.push_back(Rect(x0, y0, dimensionesROI, dimensionesROI));
+					x0 = cvRound(plaza[i].x + 3 * (x2 - plaza[i].x) / 4 - (dimensionesROI / 2));
+					ROIs2.push_back(Rect(x0, y0, dimensionesROI, dimensionesROI));
+				}
+				else
+				{
+					// Si el tamaño de la plaza es demasiado pequeño, el algoritmo no va a funcionar bien, por lo que se desecha
+					if ((plaza[i].width < dimensionesROI) || (plaza[i].height < dimensionesROI))
+					{
+						cout << "La plaza seleccionada es demasiado pequeña para el correcto funcionamiento del algoritmo." << endl;
+						cout << "Fin del programa" << endl;
+						getchar();
+						exit(EXIT_FAILURE);
+					}
+					// Solo tendríamos en este caso una ROI en el centro de la plaza
+					int x0, y0;
+					x0 = cvRound(plaza[i].x + plaza[i].width / 2 - dimensionesROI / 2);
+					y0 = cvRound(plaza[i].y + plaza[i].height / 2 - dimensionesROI / 2);
+					ROIs1.push_back(Rect(x0, y0, dimensionesROI, dimensionesROI));
+				}
 			}
 		}
 	}
 
 	// Guardamos las imágenes de los ROIs cuando el parking está vacío
-	Mat imagen_ROI1_libre(frame_gray, ROI1);
-	medianBlur(imagen_ROI1_libre, imagen_ROI1_libre, 3);
-	Mat imagen_ROI2_libre(frame_gray, ROI2);
-	medianBlur(imagen_ROI2_libre, imagen_ROI2_libre, 3);
-	imshow("ROI1", imagen_ROI1_libre);
-	imshow("ROI2", imagen_ROI2_libre);
+	vector<Mat> fondo_ROI1;
+	vector<Mat> fondo_ROI2;
+	for (int i = 0; i < numeroPlazas; i++)
+	{
+		fondo_ROI1.push_back(Mat(frame_gray, ROIs1[i]));
+		medianBlur(fondo_ROI1[i], fondo_ROI1[i], 3);
+		fondo_ROI2.push_back(Mat(frame_gray, ROIs2[i]));
+		medianBlur(fondo_ROI2[i], fondo_ROI2[i], 3);
+	}
 	
 	/* VIGILANCIA DEL APARCAMIENTO */
 
-	Mat imagen_ROI1, imagen_ROI2, frame_vigilancia, frame_vigilancia_gray;
+	vector<Mat> imagen_ROI1, imagen_ROI2;
+	Mat frame_vigilancia, frame_vigilancia_gray;
 	double norm1, norm2;
-	bool plazaOcupada;
+	bool plazaOcupada = false;
 	Scalar rojo = Scalar(0, 0, 255);
 	Scalar verde = Scalar(0, 255, 0);
+	Scalar mean, stddev;
 	namedWindow("Vigilancia", 1);
 
 	cout << endl << "Plazas seleccionadas, procediendo a la vigilancia del aparcamiento..." << endl << endl;
@@ -229,50 +247,54 @@ int main()
 		GaussianBlur(frame_vigilancia, frame_vigilancia, Size(3, 3), 0, 0);
 		cvtColor(frame_vigilancia, frame_vigilancia_gray, CV_BGR2GRAY);
 
-		// Para cada ROI actualizamos las imágenes y calculamos la "norma relativa diferencial"
-		imagen_ROI1 = Mat(frame_vigilancia_gray, ROI1);
-		medianBlur(imagen_ROI1, imagen_ROI1, 3);
-		imagen_ROI2 = Mat(frame_vigilancia_gray, ROI2);
-		medianBlur(imagen_ROI2, imagen_ROI2, 3);
-
-		norm1 = norm(imagen_ROI1_libre, imagen_ROI1, NORM_L2);
-		cout << "Norma 1: " << norm1 << endl;
-		norm2 = norm(imagen_ROI2_libre, imagen_ROI2, NORM_L2);
-		cout << "Norma 2: " << norm2 << endl;
-
-		// Si una de estas normas supera el umbral, asumimos la presencia de un coche
-		if ((norm1 > umbralPresencia) || (norm2 > umbralPresencia))
+		for (int i = 0; i < numeroPlazas; i++)
 		{
-			cout << "Plaza ocupada" << endl;
-			plazaOcupada = true;
-		}
-		else
-		{
-			cout << "Plaza libre" << endl;
-			plazaOcupada = false;
-		}
+			// Para cada ROI actualizamos las imágenes y calculamos la "norma relativa diferencial"
+			imagen_ROI1.push_back(Mat(frame_vigilancia_gray, ROIs1[i]));
+			//medianBlur(imagen_ROI1, imagen_ROI1, 3);
+			imagen_ROI2.push_back(Mat(frame_vigilancia_gray, ROIs2[i]));
+			//medianBlur(imagen_ROI2, imagen_ROI2, 3);
 
-		// Dibujamos las ROIs y la plaza
-		if (norm1 > umbralPresencia)
-			rectangle(frame_vigilancia, ROI1, rojo);
-		else
-			rectangle(frame_vigilancia, ROI1, verde);
-		// Dependiendo del tamaño del aparcamiento puede ser que no haya ROI2
-		if (ROI2.area() > 0)
-		{
-			if (norm2 > umbralPresencia)
-				rectangle(frame_vigilancia, ROI2, rojo);
+			norm1 = norm(fondo_ROI1[i], imagen_ROI1[i], NORM_L2);
+			cout << "Norma 1: " << norm1 << endl;
+			norm2 = norm(fondo_ROI2[i], imagen_ROI2[i], NORM_L2);
+			cout << "Norma 2: " << norm2 << endl;
+
+			// Si una de estas normas supera el umbral, asumimos la presencia de un coche
+			if ((norm1 > umbralPresencia) || (norm2 > umbralPresencia))
+			{
+				cout << "Plaza ocupada" << endl;
+				plazaOcupada = true;
+			}
 			else
-				rectangle(frame_vigilancia, ROI2, verde);
-		}
+			{
+				cout << "Plaza libre" << endl;
+				plazaOcupada = false;
+			}
 
-		if (plazaOcupada == true)
-			rectangle(frame_vigilancia, plaza, rojo);
-		else
-			rectangle(frame_vigilancia, plaza, verde);
+			// Dibujamos las ROIs y la plaza
+			if (norm1 > umbralPresencia)
+				rectangle(frame_vigilancia, ROIs1[i], rojo);
+			else
+				rectangle(frame_vigilancia, ROIs1[i], verde);
+			// Dependiendo del tamaño del aparcamiento puede ser que no haya ROI2
+			if (ROIs2[i].area() > 0)
+			{
+				if (norm2 > umbralPresencia)
+					rectangle(frame_vigilancia, ROIs2[i], rojo);
+				else
+					rectangle(frame_vigilancia, ROIs2[i], verde);
+			}
+
+			if (plazaOcupada == true)
+				rectangle(frame_vigilancia, plaza[i], rojo);
+			else
+				rectangle(frame_vigilancia, plaza[i], verde);
+		}
 
 		// Mostramos la imagen del aparcamiento captada por la cámara
 		imshow("Vigilancia", frame_vigilancia);
+		//imshow("motion", movimiento);
 
 		// Actualizamos el frame cada 1 segundo. Salimos del programa pulsando la tecla esc
 		esc = waitKey(30);
@@ -299,15 +321,23 @@ void onMouse(int event, int x, int y, int, void*)
 	case CV_EVENT_LBUTTONDOWN:
 		pulsar = Point(x, y);
 		break;
+	case CV_EVENT_MOUSEMOVE:
+		seleccion.x = MIN(pulsar.x, x);
+		seleccion.y = MIN(pulsar.y, y);
+		seleccion.width = std::abs(pulsar.x - x);
+		seleccion.height = std::abs(pulsar.y - y);
+		rectangle(frame, seleccion, Scalar(0, 255, 0));
+		break;
 	// Evento soltar botón del ratón
 	case CV_EVENT_LBUTTONUP:
 		soltar = Point(x, y);
 		// Construimos la plaza de aparcamiento
-		plaza.x = MIN(pulsar.x, soltar.x);
-		plaza.y = MIN(pulsar.y, soltar.y);
-		plaza.width = std::abs(pulsar.x - soltar.x);
-		plaza.height = std::abs(pulsar.y - soltar.y);
+		plaza[plazasSeleccionadas].x = MIN(pulsar.x, soltar.x);
+		plaza[plazasSeleccionadas].y = MIN(pulsar.y, soltar.y);
+		plaza[plazasSeleccionadas].width = std::abs(pulsar.x - soltar.x);
+		plaza[plazasSeleccionadas].height = std::abs(pulsar.y - soltar.y);
 		plazaSeleccionada = true;
+		plazasSeleccionadas++;
 		break;
 	default:
 		break;
